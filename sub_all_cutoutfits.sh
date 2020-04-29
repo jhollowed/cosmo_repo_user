8b22bde765c903d430d705e5862a31a34f8d7384
Joe Hollowed <jphollowed@gmail.com>
2020-04-29 23:50:09 +0000

    adding drivers for creating NFW WL mocks

---
+++ sub_all_cutoutfits.sh
@@ -0,0 +1,90 @@
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 28
+#~/anaconda3/bin/python ./make_lensing_mocks.py 28
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=28)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 26
+#~/anaconda3/bin/python ./make_lensing_mocks.py 26
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=26)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 24
+#~/anaconda3/bin/python ./make_lensing_mocks.py 24
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=24)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 22
+#~/anaconda3/bin/python ./make_lensing_mocks.py 22
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=22)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 20
+#~/anaconda3/bin/python ./make_lensing_mocks.py 20
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=20)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 18
+#~/anaconda3/bin/python ./make_lensing_mocks.py 18
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=18)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 16
+#~/anaconda3/bin/python ./make_lensing_mocks.py 16
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=16)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 14
+#~/anaconda3/bin/python ./make_lensing_mocks.py 14
+cd ~/repos/gammaProf/gammaProf
+~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=14)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 12
+#~/anaconda3/bin/python ./make_lensing_mocks.py 12
+#cd ~/repos/gammaProf/gammaProf
+~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=12)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 10
+#~/anaconda3/bin/python ./make_lensing_mocks.py 10
+#cd ~/repos/gammaProf/gammaProf
+~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=10)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 8
+#~/anaconda3/bin/python ./make_lensing_mocks.py 8
+#cd ~/repos/gammaProf/gammaProf
+~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=8)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 6
+#~/anaconda3/bin/python ./make_lensing_mocks.py 6
+#cd ~/repos/gammaProf/gammaProf
+~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=6)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 4
+#~/anaconda3/bin/python ./make_lensing_mocks.py 4
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=4)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 2
+#~/anaconda3/bin/python ./make_lensing_mocks.py 2
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=2)"
+
+#cd ~/repos/clusterMultiPlanesRayTracing
+#~/anaconda3/bin/python ./raytrace_them_all.py 0
+#~/anaconda3/bin/python ./make_lensing_mocks.py 0
+#cd ~/repos/gammaProf/gammaProf
+#~/anaconda3/bin/python -c "import example_run as f;f.sim_example_run(max_planes=0)"
+
