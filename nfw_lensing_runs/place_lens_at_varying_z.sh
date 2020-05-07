6e8e138fbd4d7279f6f0867f544bb39e84b102e8
Joe Hollowed <jphollowed@gmail.com>
2020-05-07 02:24:50 +0000

    updated sh scripts

--- nfw_lensing_runs/place_lens_at_varying_z.sh
+++ nfw_lensing_runs/place_lens_at_varying_z.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 PY=/miniconda3/bin/python
+$PY  make_and_raytrace_nfwBall.py 0.01
 $PY  make_and_raytrace_nfwBall.py 0.1
-$PY  make_and_raytrace_nfwBall.py 0.2
 $PY  make_and_raytrace_nfwBall.py 0.3
-$PY  make_and_raytrace_nfwBall.py 0.4
+$PY  make_and_raytrace_nfwBall.py 0.5
+$PY  make_and_raytrace_nfwBall.py 0.7
+$PY  make_and_raytrace_nfwBall.py 0.9
