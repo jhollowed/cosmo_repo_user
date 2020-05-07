cafa1b0c249fe071c14c0fd0ace69892bd9e0e45
Joe Hollowed <jphollowed@gmail.com>
2020-05-07 00:02:33 +0000

    adding cooley script

---
+++ nfw_lensing_runs/place_lens_at_varying_z__cooley.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PY = /home/hollowed/anaconda3/bin/python
+$PY  make_and_raytrace_nfwBall.py 0.1
+$PY  make_and_raytrace_nfwBall.py 0.2
+$PY  make_and_raytrace_nfwBall.py 0.3
+$PY  make_and_raytrace_nfwBall.py 0.4
