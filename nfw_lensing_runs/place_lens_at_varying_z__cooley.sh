8ea66d58d479a3da9ebb761a76f9257c45c93de0
Joe Hollowed <jphollowed@gmail.com>
2020-05-26 00:24:57 +0000

    updating test outputs

--- nfw_lensing_runs/place_lens_at_varying_z__cooley.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PY=/home/hollowed/anaconda3/bin/python
-$PY  make_and_raytrace_nfwBall.py 0.01
-$PY  make_and_raytrace_nfwBall.py 0.1
-$PY  make_and_raytrace_nfwBall.py 0.3
-$PY  make_and_raytrace_nfwBall.py 0.5
-$PY  make_and_raytrace_nfwBall.py 0.7
-$PY  make_and_raytrace_nfwBall.py 0.9
