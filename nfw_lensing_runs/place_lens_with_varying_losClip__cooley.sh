4be92c4178b7a95d0048ca8d4e47c5be28470fc0
Joe Hollowed <jphollowed@gmail.com>
2020-06-06 22:23:33 -0400

    adding new run script for los clipping convergence test

---
+++ nfw_lensing_runs/place_lens_with_varying_losClip__cooley.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PY=/home/hollowed/anaconda3/bin/python
+OUT=/projects/DarkUniverse_esp/jphollowed/profile_fitting_tests/convergence_tests
+
+# Calling format is make_and_raytrace_NFWBall.py zl zs N rfrac nsrcs out_dir vis_flag
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 0.2
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 0.4
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 0.6
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 0.7
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 0.8
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 1.0
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 1.2
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 1.4
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 1.6
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 1.8
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 2.0
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 3.0
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 4.0
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 5.0
+$PY  make_and_raytrace_nfwBall.py 0.2  1.0 20000 6 2000 $OUT/vary_los 6.0
+
