b64e516f0658dcf2cabb66d701965c0fc749fa86
Joe Hollowed <jphollowed@gmail.com>
2020-06-27 23:29:17 -0400

    some changes

--- nfw_lensing_runs/place_pointmass_at_varying_z__cooley.sh
+++ nfw_lensing_runs/place_pointmass_at_varying_z__cooley.sh
@@ -4,7 +4,7 @@ PY=/miniconda3/bin/python
 OUT=/Users/joe/repos/repo_user/nfw_lensing_runs/output_pointmass
 
 # Calling format is make_and_raytrace_NFWBall.py zl zs fov_size nsrcs lenspix out_dir vis_flag
-$PY make_and_raytrace_pointMass.py 0.35 0.8 1 500 1024 $OUT/vary_zl 1 dtfe grid
+$PY make_and_raytrace_pointMass.py 0.35 0.8 1 500 1024 $OUT/vary_zl 1 sph grid
 
 #$PY make_and_raytrace_pointMass.py 0.05 1.0 1 500 1024 $OUT/vary_zl 1
 #$PY make_and_raytrace_pointMass.py 0.1 1.0 1 500 1024 $OUT/vary_zl 1
